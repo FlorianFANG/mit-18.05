9c5d27d7274746c0da1fe752487592c592fe6d9b
fangyoujun <fangyoujun@momenta.ai>
2019-10-18 13:42:29 +0800

    ps1 update

--- ps1_birthday.py
+++ ps1_birthday.py
@@ -2,16 +2,17 @@ import numpy as np
 
 ntrials = 10000
 
-def sim_PB():
-    print(f'P(B) simulation')
+def birthday_match(match_num: int):
+    print(f'\nSimulation for {match_num} match:', flush=True)
     for n in range(1,100):
         samples = np.random.randint(1,366,size=(ntrials, n))
         count = 0
         for trial in range(1, ntrials):
-            u = np.unique(samples[trial])
-            if u.shape[0] < n:
+            u,c = np.unique(samples[trial], return_counts=True)
+            if match_num in c:
                 count += 1
         print(f'n = {n}: P(B) = {round(count/ntrials,5)}', flush=True)
 
 if __name__ == '__main__':
-    sim_PB()
+    birthday_match(2)
+    birthday_match(3)
